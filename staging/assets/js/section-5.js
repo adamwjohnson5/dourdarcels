1b91144c629ce40782821104e951be499b775b3a
Adam Johnson <adamjohnson@apple.com>
2022-01-14 19:58:15 +1100

    Timeline nav

--- staging/assets/js/section-5.js
+++ staging/assets/js/section-5.js
@@ -4,6 +4,43 @@
 
 /* Section 5 */
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Scroll
+    const wrapper = document.querySelector('#section-5 .section-wrapper');
+
+    wrapper.addEventListener('scroll', () => {
+        setTimelineNav(wrapper.scrollLeft);
+    });
+
+    setTimelineNav(0);
+});
+
+function setTimelineNav(pos) {
+    const timeline = document.querySelector('#section-5-timeline');
+    const contentWidth = timeline.offsetLeft + timeline.offsetWidth - window.innerWidth;
+    const contentDivided = contentWidth / 6;
+
+    // Reset all
+    const navItems = document.querySelectorAll('#timeline-nav a');
+
+    for (let x = 0; x < navItems.length; x++) {
+        document.querySelector('a#timeline-nav-' + (x + 1)).style.opacity = 1;
+    }
+
+    // Select item
+    var item = 1;
+
+    if (pos >= contentDivided * 5) {
+        item = 4;
+    } else if (pos >= contentDivided * 3) {
+        item = 3;
+    } else if (pos >= contentDivided) {
+        item = 2
+    }
+
+    document.querySelector('a#timeline-nav-' + item).style.opacity = 0.25;
+}
+
 function moveTimeline(pos) {
     const timeline = document.querySelector('#section-5-timeline');
     const contentWidth = timeline.offsetLeft + timeline.offsetWidth - window.innerWidth;
