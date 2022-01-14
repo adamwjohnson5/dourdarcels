c02ba5c54c63800fbb886c6315863350f154a6f1
Adam Johnson <adamjohnson@apple.com>
2022-01-14 15:34:56 +1100

    Timeline nav

--- staging/assets/js/section-5.js
+++ staging/assets/js/section-5.js
@@ -3,3 +3,14 @@
 /* Global vars */
 
 /* Section 5 */
+
+function moveTimeline(pos) {
+    const timeline = document.querySelector('#section-5-timeline');
+    const contentWidth = timeline.offsetLeft + timeline.offsetWidth - window.innerWidth;
+    const contentDivided = contentWidth / 3;
+
+    document.querySelector('#section-5 .section-wrapper').scrollTo({
+        left: pos === 3 ? contentWidth : pos * contentDivided,
+        behavior: 'smooth'
+    });
+}
