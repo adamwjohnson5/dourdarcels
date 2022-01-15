106ea5eb894b22283603053bd5d353dae9e0dd4b
Adam Johnson <adamjohnson@apple.com>
2022-01-15 11:51:41 +1100

    Timeline links

--- staging/assets/js/components.js
+++ staging/assets/js/components.js
@@ -3,6 +3,19 @@
 export class TimelineEvent extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<div class="event-pupil"></div><div class="event event-${ this.getAttribute('pos') }"><div class="event-line"></div><h4><span class="big-button">${ this.getAttribute('name') }</span></h4><img src="assets/img/${ this.getAttribute('src') }" alt="${ this.getAttribute('name') }" /></div>`;
+
+        // Has link
+        if (this.getAttribute('href')) {
+            this.classList.add('timeline-event-link');
+
+            this.querySelector('span.big-button').addEventListener('click', (e) => {
+                window.open(this.getAttribute('href'), '_blank');
+            });
+
+            this.querySelector('img').addEventListener('click', (e) => {
+                window.open(this.getAttribute('href'), '_blank');
+            });
+        }
     }
 }
 
