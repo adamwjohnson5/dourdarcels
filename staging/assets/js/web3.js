ffa6f78917d927e8ec44cc8e1117a1593878cdff
Adam Johnson <adamjohnson@apple.com>
2022-01-20 12:18:07 +1100

    Tweaks

--- staging/assets/js/web3.js
+++ staging/assets/js/web3.js
@@ -4,6 +4,8 @@
 
 setMintProgress(0); // Should be called on init with count of NFTs minted
 
+//toggleOverlay('A Heading', 'Some text') // Can be used to show an alert. Second var can contain HTML
+
 function connectWallet() {
     // Devs do your magic
     walletConnected(); // Please call when wallet successfully connected
