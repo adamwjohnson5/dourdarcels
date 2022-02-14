ffb8aec1b9dcd79b385384a40f70fad2190bad9f
Mayada <mayada@hexbite.com>
2022-02-14 13:32:37 -0700

    added an error handler incase the connected network is different that contract network

--- staging/assets/js/mint-wc.js
+++ staging/assets/js/mint-wc.js
@@ -314,7 +314,10 @@ async function fetchAccountData() {
   // Load chain information over an HTTP API
   const chainData = evmChains.getChain(chainId);
   document.querySelector("#network-name").textContent = chainData.name;
-
+  if (chainId !== contractNetwork) {
+    createAlert('Failed', 'Incorrect Network');
+    return;
+  }
   // Get list of accounts of the connected wallet
   const accounts = await web3.eth.getAccounts();
 
