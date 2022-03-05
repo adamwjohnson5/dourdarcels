1a5a1f93bbb36752f88fc42b036602e9b3a5b60a
Adam Johnson <adamjohnson@apple.com>
2022-03-06 10:09:36 +1100

    Update section-2.js

--- assets/js/section-2.js
+++ assets/js/section-2.js
@@ -68,11 +68,11 @@ function walletConnected() {
     // Must be whitelisted if pre-sale
     if (!window.presale || window.presale && window.whitelisted) {
         // Minting buttons
-        const mintingQuantity = document.querySelector('#minting-button-2');
+        /* const mintingQuantity = document.querySelector('#minting-button-2');
         mintingQuantity.style.opacity = 1;
         mintingQuantity.querySelector('a#button-2-minus').style.pointerEvents = 'auto';
         mintingQuantity.querySelector('a#button-2-plus').style.pointerEvents = 'auto';
-        document.querySelector('#minting-button-3').style.opacity = 1;
+        document.querySelector('#minting-button-3').style.opacity = 1; */
     }
 
     setMintingDetails();
