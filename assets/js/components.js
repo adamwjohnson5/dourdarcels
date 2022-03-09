1dcfc323b049fcb46bd555adf8f50585ec88e13a
Adam Johnson <adamjohnson@apple.com>
2022-03-09 12:02:40 +1100

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -2,7 +2,7 @@
 
 export class TimelineEvent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `</div><div class="event event-${ this.getAttribute('pos') }"><div class="event-line"></div><h4><span class="big-button">${ this.getAttribute('name') }</span></h4><img src="assets/img/${ this.getAttribute('src') }" alt="${ this.getAttribute('name') }" /></div><div class="event-pupil">`;
+        this.innerHTML = `</div><div class="event event-${ this.getAttribute('pos') }"><div class="event-line"></div><h4><span class="big-button">${ this.getAttribute('name') }</span></h4><img src="/assets/img/${ this.getAttribute('src') }" alt="${ this.getAttribute('name') }" /></div><div class="event-pupil">`;
 
         // Has link
         if (this.getAttribute('href')) {
