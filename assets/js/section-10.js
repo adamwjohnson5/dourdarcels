95634f406929fa67c29ad668bf74aa6255fbf2b1
Adam Johnson <adam@adamjohnson.org>
2022-05-25 18:50:15 +1000

    Update section-10.js

--- assets/js/section-10.js
+++ assets/js/section-10.js
@@ -64,12 +64,21 @@ async function walletConnectedPortal() {
 }
 
 function test() {
-    alert('Test')
+
+    function dataURLtoBlob(dataurl) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {type:mime});
+    }
+
     if (navigator.share) {
 
         navigator.share({
             title: 'Test',
-          url: 'https://dourdarcels.io'
+          files: [dataURLtoBlob(window.imgURL)]
         }).then(() => {
           console.log('Thanks for sharing!');
         })
