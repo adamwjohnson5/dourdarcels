f3376c31fa61a0878971b56bb7cb28c78d35cb79
Adam Johnson <adam@adamjohnson.org>
2022-06-02 09:41:24 +1000

    Update section-9.js

--- assets/js/section-9.js
+++ assets/js/section-9.js
@@ -34,8 +34,14 @@ async function validateMerch9(form) {
         // Get DD from wallet using Alchemy
         const darcels = await getData(`https://eth-mainnet.alchemyapi.io/v2/kA0GvyDvzb_9brFE0cU4YM5cKdbdmWe9/getNFTs/?owner=${ address }&contractAddresses[]=0x8d609bd201beaea7dccbfbd9c22851e23da68691`);
         const nfts = darcels.ownedNfts;
+        const colette = await getData(`https://eth-mainnet.alchemyapi.io/v2/kA0GvyDvzb_9brFE0cU4YM5cKdbdmWe9/getNFTs/?owner=${ address }&contractAddresses[]=0x6d93d3fd7bb8baebf853be56d0198989db655e40`);
 
-        if (nfts.length > 0) {
+        // All Colette owners eligible
+        if (colette.ownedNfts.length > 0) {
+            verified = true;
+        }
+
+        if (nfts.length > 0 && !verified) {
             // Loop tokens
             for (let x = 0; x < nfts.length; x++) {
                 let traits = nfts[x].metadata.attributes;
